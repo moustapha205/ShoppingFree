a049520f6ebea25746b8697ab1262ffa41a32c61
Moustapha Kane <kanem1468@mail.com>
2026-04-01 09:45:56 +0200

    Fix FOREIGN KEY syntax for MySQL 8.0

--- migrations/Version20260105204824.php
+++ migrations/Version20260105204824.php
@@ -20,7 +20,7 @@ final class Version20260105204824 extends AbstractMigration
         $this->addSql('CREATE TABLE IF NOT EXISTS eleve (id INT AUTO_INCREMENT NOT NULL, auto_ecole_id INT DEFAULT NULL, nom_eleve VARCHAR(50) NOT NULL, prenom_eleve VARCHAR(50) NOT NULL, tel_eleve VARCHAR(20) NOT NULL, date_inscription DATE NOT NULL, naissance_eleve DATE DEFAULT NULL, INDEX IDX_ECA105F7B1C987E1 (auto_ecole_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE IF NOT EXISTS product (id INT AUTO_INCREMENT NOT NULL, sku VARCHAR(50) NOT NULL, name VARCHAR(255) NOT NULL, prix_ht NUMERIC(10, 2) NOT NULL, vat_rate NUMERIC(3, 2) NOT NULL, quantity INT NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE IF NOT EXISTS user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('ALTER TABLE eleve ADD CONSTRAINT FK_ECA105F7B1C987E1 FOREIGN KEY IF NOT EXISTS (auto_ecole_id) REFERENCES auto_ecole (id)');
+        $this->addSql('ALTER TABLE eleve ADD CONSTRAINT FK_ECA105F7B1C987E1 FOREIGN KEY (auto_ecole_id) REFERENCES auto_ecole (id)');
     }
 
     public function down(Schema $schema): void
